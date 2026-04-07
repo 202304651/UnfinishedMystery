9efd080b06489998737025bf1d26e5f28f05ae8b
Fatema Maitham <fatemamaitham1@gmail.com>
2026-04-07 20:23:28 +0300

    Fix the text

--- Assets/scripts/MainMenu/FullscreenButton.cs
+++ Assets/scripts/MainMenu/FullscreenButton.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FullscreenButton : MonoBehaviour
 {
+    // Assign these in the Inspector
     public Button fullscreenButton;
-    public Text buttonText;
-
+    public TMP_Text buttonText;
     void Start()
     {
-        // Set button text to current fullscreen state
+        // Set initial text based on current fullscreen state
         UpdateButtonText();
 
         // Add click listener
@@ -17,12 +18,16 @@ public class FullscreenButton : MonoBehaviour
 
     void ToggleFullscreen()
     {
+        // Switch fullscreen on/off
         Screen.fullScreen = !Screen.fullScreen;
+
+        // Update button text
         UpdateButtonText();
     }
 
     void UpdateButtonText()
     {
+        // Short ON/OFF text since you already have a label
         buttonText.text = Screen.fullScreen ? "ON" : "OFF";
     }
 }
